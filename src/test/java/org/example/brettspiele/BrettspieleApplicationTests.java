e5abb034d0c317bff3b7c9d827ee0d27ae18bcbc
basak-tahtaci <basaktahtaci@MacBook-Air-von-Basak.local>
2026-09-14 20:59:03 +0200

    initial commit

---
+++ src/test/java/org/example/brettspiele/BrettspieleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.brettspiele;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrettspieleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
